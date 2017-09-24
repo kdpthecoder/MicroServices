867dc43b9bbacab15862c064ab3742d55ac6ad45
Kshitij <Kshitij@kshitij>
2017-09-24 22:48:23 +0530

    Initial Commit

---
+++ DataCleansingService/src/main/java/com/cts/aim/microservices/DataCleansingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cts.aim.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCleansingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCleansingServiceApplication.class, args);
+	}
+}
